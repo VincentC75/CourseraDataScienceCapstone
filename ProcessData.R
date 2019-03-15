eb98efc513b443ccd749c5ebf37437cd17741151
VincentC75 <vicourtois@orange.fr>
2019-03-15 18:04:52 +0100

    Add up to 5-grams.

--- ProcessData.R
+++ ProcessData.R
@@ -18,6 +18,7 @@ rm(en_blog, en_twitter, en_news)
 # Build corpus
 library(quanteda)
 library(dplyr)
+library(tidyr)
 en_corpus <- corpus(en_text)
 
 
@@ -26,7 +27,7 @@ unigrams <- dfm(en_corpus, ngrams = 1, concatenator = " ",remove_punct=TRUE, tol
 unigrams <- as.data.frame(as.matrix(docfreq(unigrams)))
 names(unigrams) <- c("Freq")
 unigrams$word <- rownames(unigrams)
-unigrams <- unigrams %>% arrange(-Freq) %>% filter(Freq > 50)
+unigrams <- unigrams %>% arrange(-Freq) %>% filter(Freq > 5)
 saveRDS(unigrams,"unigrams.RData")
 rm(unigrams)
 
@@ -34,16 +35,51 @@ rm(unigrams)
 bigrams <- dfm(en_corpus, ngrams = 2, concatenator = " ",remove_punct=TRUE, tolower=TRUE, remove_twitter=TRUE)
 bigrams <- as.data.frame(as.matrix(docfreq(bigrams)))
 names(bigrams) <- c("Freq")
-bigrams$word <- rownames(bigrams)
-bigrams <- bigrams %>% arrange(-Freq) %>% filter(Freq > 50)
+bigrams$words <- rownames(bigrams)
+bigrams <- bigrams %>% separate(words, c("Word1", "Word2"), ' ')
+bigrams <- bigrams %>% arrange(-Freq) %>% filter(Freq > 5)
 saveRDS(bigrams,"bigrams.RData")
 rm(bigrams)
 
 # Trigrams
-trigrams <- dfm(en_corpus, ngrams = 3, concatenator = " ",remove_punct=TRUE, tolower=TRUE, remove_twitter=TRUE)
+# All data cannot be used for trigrams, a sample must be used instead of the entire dataset
+set.seed(1971)
+en_corpus_sampled <- corpus(sample(en_text, length(en_text) * 0.3))
+trigrams <- dfm(en_corpus_sampled, ngrams = 3, concatenator = " ",remove_punct=TRUE, tolower=TRUE, remove_twitter=TRUE)
 trigrams <- as.data.frame(as.matrix(docfreq(trigrams)))
 names(trigrams) <- c("Freq")
-trigrams$word <- rownames(trigrams)
-trigrams <- trigrams %>% arrange(-Freq) %>% filter(Freq > 50)
+trigrams$words <- rownames(trigrams)
+trigrams <- trigrams %>% arrange(-Freq) %>% filter(Freq > 5)
+trigrams <- trigrams %>% separate(words, c("Word1", "Word2", "Word3"), ' ')
 saveRDS(trigrams,"trigrams.RData")
 rm(trigrams)
+
+# Quadrigrams
+quadrigrams <- dfm(en_corpus_sampled, ngrams = 4, concatenator = " ",remove_punct=TRUE, tolower=TRUE, remove_twitter=TRUE)
+quadrigrams <- as.data.frame(as.matrix(docfreq(quadrigrams)))
+names(quadrigrams) <- c("Freq")
+quadrigrams$words <- rownames(quadrigrams)
+quadrigrams <- quadrigrams %>% arrange(-Freq) %>% filter(Freq > 5)
+quadrigrams <- quadrigrams %>% separate(words, c("Word1", "Word2", "Word3", "Word4"), ' ')
+saveRDS(quadrigrams,"quadrigrams.RData")
+rm(quadrigrams)
+
+# Pentagrams
+pentagrams <- dfm(en_corpus_sampled, ngrams = 5, concatenator = " ",remove_punct=TRUE, tolower=TRUE, remove_twitter=TRUE)
+pentagrams <- as.data.frame(as.matrix(docfreq(pentagrams)))
+names(pentagrams) <- c("Freq")
+pentagrams$words <- rownames(pentagrams)
+pentagrams <- pentagrams %>% arrange(-Freq) %>% filter(Freq > 5)
+pentagrams <- pentagrams %>% separate(words, c("Word1", "Word2", "Word3", "Word4", "Word5"), ' ')
+saveRDS(pentagrams,"pentagrams.RData")
+rm(pentagrams)
+
+
+# Load saved data
+rm(en_corpus)
+rm(en_corpus_sampled)
+unigrams <- readRDS("unigrams.RData")
+bigrams <- readRDS("bigrams.RData")
+trigrams <- readRDS("trigrams.RData")
+quadrigrams <- readRDS("quadrigrams.RData")
+pentaframs <- readRDS("pentagrams.RData")
