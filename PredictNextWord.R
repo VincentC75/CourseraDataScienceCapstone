155d8055cf77d4282860f9c93d46db236356fd75
VincentC75 <vicourtois@orange.fr>
2019-03-16 12:06:03 +0100

    Clean input to prediction function.

--- PredictNextWord.R
+++ PredictNextWord.R
@@ -8,7 +8,15 @@ quadrigrams <- readRDS("quadrigrams.RData")
 pentagrams <- readRDS("pentagrams.RData")
 hexagrams <- readRDS("hexagrams.RData")
 
+CleanInput <- function(inputwords) {
+  inputwords <- tolower(inputwords)
+  inputwords <- gsub("#|\\?|;|!|:|\\)|\\(|\\.|&|,", "", inputwords)
+  inputwords <- trimws(inputwords)
+  inputwords
+}
+
 PredictNextWord <- function(prevwords) {
+  prevwords <- CleanInput(prevwords)
   startwords <- unlist(strsplit(prevwords, split = ' '))
   
   # last resort if no match found: predict the 3 most common unigrams
@@ -23,7 +31,7 @@ PredictNextWord <- function(prevwords) {
                              hexagrams$Word2 == startwords[l-3] & 
                              hexagrams$Word1 == startwords[l-4]
                                , c('Word6', 'Freq')]
-      if (length(candidate) > 0) {
+      if (nrow(candidate) > 0) {
         candidate$Word <- candidate$Word6
         candidate$Word6 <- NULL
         candidate$Matched <- 5
@@ -37,7 +45,7 @@ PredictNextWord <- function(prevwords) {
                              pentagrams$Word2 == startwords[l-2] & 
                              pentagrams$Word1 == startwords[l-3]
                            , c('Word5', 'Freq')]
-    if (length(candidate) > 0) {
+    if (nrow(candidate) > 0) {
       candidate$Word <- candidate$Word5
       candidate$Word5 <- NULL
       candidate$Matched <- 4
@@ -50,7 +58,7 @@ PredictNextWord <- function(prevwords) {
                              quadrigrams$Word2 == startwords[l-1] & 
                              quadrigrams$Word1 == startwords[l-2]
                            , c('Word4', 'Freq')]
-    if (length(candidate) > 0) {
+    if (nrow(candidate) > 0) {
       candidate$Word <- candidate$Word4
       candidate$Word4 <- NULL
       candidate$Matched <- 3
@@ -62,7 +70,7 @@ PredictNextWord <- function(prevwords) {
     candidate <- trigrams[trigrams$Word2 == startwords[l] & 
                                trigrams$Word1 == startwords[l-1]
                              , c('Word3', 'Freq')]
-    if (length(candidate) > 0) {
+    if (nrow(candidate) > 0) {
       candidate$Word <- candidate$Word3
       candidate$Word3 <- NULL
       candidate$Matched <- 2
@@ -73,7 +81,7 @@ PredictNextWord <- function(prevwords) {
   if (l >= 1) {
     candidate <- bigrams[bigrams$Word1 == startwords[l]
                           , c('Word2', 'Freq')]
-    if (length(candidate) > 0) {
+    if (nrow(candidate) > 0) {
       candidate$Word <- candidate$Word2
       candidate$Word2 <- NULL
       candidate$Matched <- 1
@@ -82,6 +90,6 @@ PredictNextWord <- function(prevwords) {
     }
   }
 #  prediction %>% arrange(-Matched, -Freq) %>% head(3) %>% select(Word)
-  prediction %>% arrange(-Matched, -Freq) %>% group_by(Word) %>% summarise(Matched = max(Matched), Freq = first(Freq)) %>% arrange(-Matched, -Freq) %>% select(Word) %>% head(3)
+  prediction %>% arrange(-Matched, -Freq) %>% group_by(Word) %>% summarise(Matched = max(Matched), Freq = first(Freq)) %>% arrange(-Matched, -Freq) %>% select(Word) %>% head(3) %>% unlist()
 }
 
